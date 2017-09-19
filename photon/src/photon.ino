04081ade143a1baaf010d6ac76c5a5f6834c7091
James Kim <james@firstbuild.com>
2017-09-19 16:36:56 -0400

    docs: Define reasons for hard coded values

--- photon/src/photon.ino
+++ photon/src/photon.ino
@@ -5,12 +5,16 @@
  * Date:
  */
 
+ // Pins
+ #define pump D1
+
+ // Output device parameters
  #define dispenseTimeForOneShot 800
 
 // setup() runs once, when the device is first turned on.
 void setup() {
-  // Put initialization like pinMode and begin functions here.
-  pinMode(D1, OUTPUT);
+  // Put initialization like pinMode and begin function here.
+  pinMode(pump, OUTPUT);
   delay(1000);
 }
 
@@ -20,7 +24,13 @@ void loop() {
 }
 
 void dispenseLiquid() {
-  analogWrite(D1, 194);
+  //  Given that the mosfet range is [0, 5.0], D1 is rated only up to
+  //    3.3v. Therefore, we are able to achieve 50% duty cycle by
+  //    writing 194.
+  //  Calculations: 194 / 255 = 76%
+  //                .76 * 3.3 = 2.5
+  //  50% duty cycle is achieved with a 2.5v mosfet signal output.
+  analogWrite(pump, 194);
   delay(800);
-  analogWrite(D1, 0);
+  analogWrite(pump, 0);
 }

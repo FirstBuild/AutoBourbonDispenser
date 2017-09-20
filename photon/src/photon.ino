549c9426b8ac418deb348ff22e76c2ac2237c30a
James Kim <james@firstbuild.com>
2017-09-19 21:05:30 -0400

    feat: Implement support for HX711 and load cell

--- photon/src/photon.ino
+++ photon/src/photon.ino
@@ -5,22 +5,68 @@
  * Date:
  */
 
+ #include "HX711ADC/HX711ADC.h"
+
  // Pins
  #define pump D1
+ #define dout A1
+ #define clk A0
 
  // Output device parameters
  #define dispenseTimeForOneShot 800
 
 // setup() runs once, when the device is first turned on.
+//void setup() {
+//  // Put initialization like pinMode and begin function here.
+//  pinMode(pump, OUTPUT);
+//  delay(1000);
+//
+//}
+
+HX711ADC *scale = NULL;
+
+int calibration_factor = 14000; //-7050 worked for my 440lb max scale setup
+
 void setup() {
-  // Put initialization like pinMode and begin function here.
-  pinMode(pump, OUTPUT);
-  delay(1000);
+  Serial.begin(9600);
+  Serial.println("HX711 calibration sketch");
+  Serial.println("Remove all weight from scale");
+  Serial.println("After readings begin, place known weight on scale");
+  Serial.println("Press + or a to increase calibration factor");
+  Serial.println("Press - or z to decrease calibration factor");
+
+  scale = new HX711ADC(7, 6);
+  scale->set_scale(calibration_factor);
+
+  scale->tare();	//Reset the scale to 0
+
+
+  long zero_factor = scale->read_average(); //Get a baseline reading
+  Serial.print("Zero factor: "); //This can be used to remove the need to tare the scale. Useful in permanent scale projects.
+  Serial.println(zero_factor);
 }
 
-// loop() runs over and over again, as quickly as it can execute.
 void loop() {
-  // The core of your code will likely live here.
+
+  //scale->set_scale(calibration_factor); //Adjust to this calibration factor
+
+  Serial.print("Reading: ");
+  Serial.print(scale->get_units(), 1);
+  Serial.print(" lbs"); //Change this to kg and re-adjust the calibration factor if you follow SI units like a sane person
+  Serial.print(" calibration_factor: ");
+  Serial.print(calibration_factor);
+  Serial.println();
+
+  if(Serial.available())
+  {
+    char temp = Serial.read();
+    if(temp == '+' || temp == 'a')
+      calibration_factor += 10;
+    else if(temp == '-' || temp == 'z')
+      calibration_factor -= 10;
+  }
+
+  delay(200);
 }
 
 void dispenseLiquid() {
